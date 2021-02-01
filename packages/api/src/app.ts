fa041e886ed2b00f272b888a74712d8c982f7d2b
Artur Nunes <arturnunespe@gmail.com>
2021-02-01 03:39:59 -0300

    add helmet and rate limiter

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors'
 import path from 'path'
 import express from 'express'
 import socketIo from 'socket.io'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 
 import { routes } from './routes'
 import { errorHandler } from './errors'
@@ -12,6 +14,8 @@ const app = express()
 const origin = process.env.NODE_ENV === 'production' ? process.env.WEB_URL : '*'
 
 app.use(cors({ origin }))
+app.use(helmet())
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }))
 app.use(express.json())
 app.use(routes)
 app.use('/images', express.static(path.join(__dirname, '..', 'uploads')))
